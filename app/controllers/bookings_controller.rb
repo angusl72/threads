f8ab1260ef6b38a9e5d2ce0c15600279846174f6
Angus Laird <anguslaird72@gmail.com>
2022-11-26 16:30:21 +1100

    added reviews controllers and controller actions

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,18 @@
 class BookingsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def update_status
+  end
+
+  def new
+  end
+
+  def create
+  end
+
 end
---
+++ app/controllers/item_reviews_controller.rb
@@ -0,0 +1,7 @@
+class ItemReviewsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
