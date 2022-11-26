686bb7d951bb3b0efe6c57a9007199f17cad28ea
Angus Laird <anguslaird72@gmail.com>
2022-11-26 16:23:33 +1100

    added validations and routes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :item
   belongs_to :user
+  validates :status, :start_date, :end_date, :booking_price, presence: true
+  validates :status, inclusion: { in: %w[pending declined completed in-progress], message: "%{value} is not a valid item type" }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :items do
+    resources :booking, only: %i[new create]
+  end
+  resources :booking, only: %i[show index] do
+    resources :seller_reviews, only: %i[new create]
+    resources :item_reviews, only: %i[new create]
+  end
 end
