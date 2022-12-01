9cae9e96a26b579c954de14f2c09348bf8827814
Angus Laird <anguslaird72@gmail.com>
2022-12-01 21:20:00 +1100

    added view and actions for new and create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,9 +15,16 @@ class ItemsController < ApplicationController
   end
 
   def create
-    authorize @item
     @item = Item.new(item_params)
     @item.user = current_user
+
+    authorize @item
+
+    if @item.save
+      redirect_to @item, notice: 'item created'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -40,7 +47,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :type, :description, :price, :size)
+    params.require(:item).permit(:name, :type, :category, :description, :price, :size)
   end
 
   def set_item
