76a979f55f4c5f44e3d3e5b321585511768731e2
Shawn McSparron <shawnmc_28@hotmail.com>
2022-12-01 20:50:41 +1100

    new seller review form saving

--- app/controllers/seller_reviews_controller.rb
+++ app/controllers/seller_reviews_controller.rb
@@ -1,13 +1,6 @@
 class SellerReviewsController < ApplicationController
   before_action :set_seller_review, only: [:show, :edit, :update, :destroy]
-
-  def index
-
-  end
-
-  def show
-
-  end
+  before_action :set_booking, only: [:new, :create, :edit, :update, :destroy]
 
   def new
     @seller_review = SellerReview.new
@@ -15,7 +8,12 @@ class SellerReviewsController < ApplicationController
 
   def create
     @seller_review = SellerReview.new(seller_review_params)
-    #need to add redirection to item page
+    @seller_review.booking = @booking
+    if @seller_review.save!
+      redirect_to root_path, status: :see_other
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -38,8 +36,12 @@ class SellerReviewsController < ApplicationController
     @seller_review = SellerReview.find(params[:id])
   end
 
+  def set_booking
+    @booking = Booking.find(params[:booking_id])
+  end
+
   def seller_review_params
-    params.require(:seller_review).permit(:rating, :feedback)
+    params.require(:seller_review).permit(:rating, :feedback, :booking_id)
   end
 
 end
