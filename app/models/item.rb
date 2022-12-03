bf550e1ea3d9e5dfd4bb6fd4b3c31b5a07fefafe
ahmet menevse <ahmetmenevse26@gmail.com>
2022-12-03 08:37:11 +0800

    fixed edit and destroy method for seller

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
   validates :name, :category, :description, :price, presence: true
   validates :category, inclusion: { in: %w[tshirt pants top shirt jeans shoes], message: "%{value} is not a valid clothing category" }
 end
