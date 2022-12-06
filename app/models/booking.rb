5da7f03528ea57e35a24e5f0b6b8025a905dac90
Shawn McSparron <shawnmc_28@hotmail.com>
2022-12-06 19:56:20 +1100

    removed logic from bookings view to controller and edited the seeds file

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :item
   belongs_to :user
   validates :status, :start_date, :end_date, :booking_price, presence: true
-  validates :status, inclusion: { in: %w[pending declined completed in-progress], message: "%{value} is not a valid item type" }
+  validates :status, inclusion: { in: %w[Pending Declined Completed In-progress], message: "%{value} is not a valid item type" }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,13 @@ User.destroy_all
 Item.destroy_all
 Booking.destroy_all
 
+test_user = User.create!(
+  email: "test@test.com",
+  password: "123456",
+  first_name: "Test",
+  last_name: "Account",
+  address: "123 Test Street"
+)
 
 10.times do
   user = User.create!(
@@ -22,9 +29,9 @@ Booking.destroy_all
     puts "creating item: #{item_count}"
     img_id = user.id + item_count
     item_count += 1
-    item_category = %w[tshirt pants top shirt jeans shoes].sample
+    item_category = %w[T-Shirt Pants Top Shirt Jeans Shoes].sample
     item = Item.new(
-      name: "#{['black','red','striped','vintage','new','cottagecore','gorp','minimalist','tailored','womens'].sample} #{item_category}",
+      name: "#{['Black','Red','Striped','Vintage','New','Cottagecore','Gorp','Minimalist','Tailored','Womens'].sample} #{item_category}",
       # name: Faker::Coffee.variety,
       category: item_category,
       description: Faker::Lorem.paragraph,
@@ -38,17 +45,18 @@ Booking.destroy_all
     puts "item attached? #{item.photo.attached?}"
   end
 end
-  items = Item.all
-  items.each do |item|
-    booking = Booking.create(
-      status: %w[pending declined completed in-progress].sample,
-      start_date: Faker::Date.between(from: Date.today, to: 5.days.from_now),
-      end_date: Faker::Date.between(from: 10.days.from_now, to: 20.days.from_now),
-      booking_price: [10, 15, 20, 40, 50, 70].sample,
-      user: User.all.sample,
-      item: item
-    )
-    puts "creating booking: #{booking.id}"
-  end
+
+items = Item.all
+items.each do |item|
+  booking = Booking.create(
+    status: %w[Pending Declined Completed In-progress].sample,
+    start_date: Faker::Date.between(from: Date.today, to: 5.days.from_now),
+    end_date: Faker::Date.between(from: 10.days.from_now, to: 20.days.from_now),
+    booking_price: [10, 15, 20, 40, 50, 70].sample,
+    user: User.all.sample,
+    item: item
+  )
+  puts "creating booking: #{booking.id}"
+end
 
 puts "Finished"
