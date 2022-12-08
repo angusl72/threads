911bfd97f38cf5eada952bf784d6dd2004504730
Priyanka <priyankasah296@gmail.com>
2022-12-08 19:37:47 +1100

    changed in schema

--- db/schema.rb
+++ db/schema.rb
@@ -76,6 +76,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_08_032907) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
+  end
+
   create_table "seller_reviews", force: :cascade do |t|
     t.integer "rating"
     t.string "feedback"
